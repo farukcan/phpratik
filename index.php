03c1950cbea385c85aa8ba26aa18fb235f996dcb
farukcan <omer@farukcan.net>
2015-09-05 21:19:12 +0300

    index.php yonlendir upt

--- index.php
+++ index.php
@@ -61,12 +61,12 @@ function yonlendir($url){
     if (!headers_sent()){  
         header('Location: '.$url); exit; 
     }else{ 
-        $sayfaKod.= '<script type="text/javascript">'; 
-        $sayfaKod.= 'window.location.href="'.$url.'";'; 
-        $sayfaKod.= '</script>'; 
-        $sayfaKod.= '<noscript>'; 
-        $sayfaKod.= '<meta http-equiv="refresh" content="0;url='.$url.'" />'; 
-        $sayfaKod.= '</noscript>'; exit; 
+        echo '<script type="text/javascript">'; 
+        echo 'window.location.href="'.$url.'";'; 
+        echo '</script>'; 
+        echo '<noscript>'; 
+        echo '<meta http-equiv="refresh" content="0;url='.$url.'" />'; 
+        echo '</noscript>'; exit; 
     }
 }
 
