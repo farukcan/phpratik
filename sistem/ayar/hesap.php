e63da0874f41f6929cbcf0fa95176c518d4bbafb
farukcan <omer@farukcan.net>
2015-11-18 22:59:51 +0200

    yetkiler

--- sistem/ayar/hesap.php
+++ sistem/ayar/hesap.php
@@ -5,7 +5,9 @@ return array(
 		"usernameColumn" => "username",
 		"passwordColumn" => "password",
 		"yetkiColumn" => "yetkiler",
+		"yetkiler" = "adminlik",
 		"hash" => "sha1", // veya md5 veya yok
 
 	);
+
 
